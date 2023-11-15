82962847c3f5bad01c9d1bf523b3f66b70e50e6e
mmupfumir <mmupfumir@hartford.edu>
2023-11-15 17:28:30 -0500

    Update RunMatrix.java
    receiving first matrix size

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,9 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in)
+
+    System.out.print("Please enter the size of your matrix")
+    int size = scanner.nextInt();
   }
 }

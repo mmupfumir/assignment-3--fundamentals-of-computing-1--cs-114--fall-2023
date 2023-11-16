0f0b07a2e8493f8450d944b1bef998666b2f3011
mmupfumir <mmupfumir@hartford.edu>
2023-11-15 20:37:23 -0500

    fixed runmaatrix to print 0 as default value.

--- RunMatrix.java
+++ RunMatrix.java
@@ -4,13 +4,24 @@ public class RunMatrix {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Please enter the size of your matrix");
+    System.out.print("Please enter the size of your matrix: ");
     int size = scanner.nextInt();
 
     Matrix matrix = new Matrix(size);
 
-    System.out.println("Default matrix:");
+    System.out.println("Your matrix is" + size + "x" + size);
+    System.out.println("Printing matrix with default values:");
+    matrix.resetMatrix();
+    matrix.printMatrix();
+
+    System.out.println("Popultaing matrix...matrix populated");
+    System.out.println("Printing matrix:");
     matrix.populateMatrix();
+    matrix.printMatrix();
+
+    System.out.println("Flipping matrix...matrix flipped");
+    System.out.println("Printing flipped matrix: ");
+    matrix.flipMatrix();
     matrix.printMatrix();
 
     scanner.close();
